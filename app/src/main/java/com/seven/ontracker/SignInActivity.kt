9549bd30c26f185ec2f522c2b98864c0ee1895b6
Samarth Patel <samarthp217@gmail.com>
2021-02-04 23:36:58 -0500

    SignIn page and Firestore setup

---
+++ app/src/main/java/com/seven/ontracker/SignInActivity.kt
@@ -0,0 +1,55 @@
+package com.seven.ontracker
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+
+private val RC_SIGN_IN = 1234
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        // authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setIsSmartLockEnabled(false)
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                //send to  View recipes activity
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(intent)
+
+            } else {
+                //shows when login fails
+                Toast.makeText(this, "Invalid Login", Toast.LENGTH_LONG).show()
+            }
+
+        }
+    }
+}
